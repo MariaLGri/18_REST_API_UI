7aa4806972ebf33e7741e33ec84533906694807b
MariaLGri <maria107aa@gmail.com>
2025-07-14 10:24:56 +0300

    Update WithLoginExtension.java
    убран закомментированный код

--- src/test/java/helpers/WithLoginExtension.java
+++ src/test/java/helpers/WithLoginExtension.java
@@ -10,39 +10,6 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static tests.TestData.*;
 
-//public class WithLoginExtension implements BeforeEachCallback {
-//    @Override
-//    public void beforeEach(ExtensionContext context) {
-//        if (context.getTestMethod().isPresent() &&
-//                context.getTestMethod().get().isAnnotationPresent(WithLogin.class)) {
-//
-//            authorizeAndSetCookies();
-//        }
-//
-//    }
-//
-//    @Step("Авторизация через API и установка cookies")
-//    private void authorizeAndSetCookies() {
-//        LoginResponseModel auth = ApiHelpersLogin.loginRequest();
-//
-//
-//        userId = auth.getUserId();
-//        token = auth.getToken();
-//        expires = auth.getExpires();
-//
-//        open("/favicon.ico");
-//
-//        getWebDriver().manage().addCookie(new Cookie("userID", userId));
-//        getWebDriver().manage().addCookie(new Cookie("expires", expires));
-//        getWebDriver().manage().addCookie(new Cookie("token", token));
-//    }
-//    @Step("Обновление токена")
-//    public String refreshToken() {
-//        // Вызываем API авторизации для получения нового токена
-//        LoginResponseModel auth = ApiHelpersLogin.loginRequest();
-//        return auth.getToken();
-//    }
-//}
 
 
 public class WithLoginExtension implements BeforeEachCallback {
